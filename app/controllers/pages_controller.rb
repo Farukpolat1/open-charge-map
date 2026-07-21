0f4247a2e25817e2eb078234f495266d4aacec68
Faruk POLAT <farukpolat2309@gmail.com>
2026-07-21 14:17:08 +0300

    deliver emails later

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,16 +24,16 @@ class PagesController < ApplicationController
   def about
   end
 
-    def contact
-  if request.post?
-    ContactMailer.notify(
-      name: params[:name],
-      email: params[:email],
-      subject: params[:subject],
-      message: params[:message]
-    ).deliver_now
+  def contact
+    if request.post?
+      ContactMailer.notify(
+        name: params[:name],
+        email: params[:email],
+        subject: params[:subject],
+        message: params[:message]
+      ).deliver_later
 
-    redirect_to contact_path, notice: "Mesajınız için teşekkürler, en kısa sürede size dönüş yapacağız."
-  end
+      redirect_to contact_path, notice: "Mesajınız için teşekkürler, en kısa sürede size dönüş yapacağız."
     end
+  end
 end
